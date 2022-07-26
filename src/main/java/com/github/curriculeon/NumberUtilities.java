0822b5e26050613a9df0e99a05a2275f1d8c8694
Brent <BrentC0328@gmail.com>
2022-07-25 20:33:30 -0400

    Number Utilities

--- src/main/java/com/github/curriculeon/NumberUtilities.java
+++ src/main/java/com/github/curriculeon/NumberUtilities.java
@@ -1,36 +1,120 @@
 package com.github.curriculeon;
 
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        int x = 0;
+
+        for (x = start; x < stop; x++) {
+
+            //find out if the variable is even its even
+            int remainder = x % 2;
+            boolean isEven = (remainder == 0);
+
+            //if it is even
+            if (isEven) {
+                result = result + x;
+            }
+
+        }
+        //Once loop is finished, return
+        return result;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
-    }
+        String result = "";
+        int x = 0;
 
+        for (x = start; x < stop; x++) {
+
+            //find out if the variable is odd
+            int remainder = x % 2;
+            boolean isEven = (remainder == 0);
+
+            //if it is odd
+            if (!isEven) {
+                result = result + x;
+            }
+
+        }
+        //Once loop is finished, return
+        return result;
+    }
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String result = "";
+        //start, start < stop, (start + step)^exponent
+        for (int x = start; x < stop;x += step){
+            //Exponent to step via Math.pow (which turns this into a double
+            // little double can not turn into int but big Double can
+            Double exponential = Math.pow(x, 2);
+
+            //Have to turn back into an integer
+            Integer exponentialInt = exponential.intValue();
+
+            //result = result + exponentialInt
+            result = result + exponentialInt;
+        }
+        return result;
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        String result = "";
+        //0, stop, ++
+        for (int x = 0; x < stop; x++) {
+
+            //result = result +
+            result = result + x;
+
+            //return result range
+        }
+        return result;
+
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String result = "";
+        //start, start < stop, start++
+        for (int x = start; x < stop; x++){
+            //result = result + x
+            result = result + x;
+        }
+
+        return result;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String result = "";
+        //start, start < stop, start + step (x = x + step)
+        for (int x = start; x < stop; x += step) {
+            //result = result + x
+            result = result + x;
+        }
+        return result;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
-    }
+        String result = "";
+        //start, start < stop, (start + step)^exponent
+        for (int x = start; x < stop;x += step){
+            //Exponent to step via Math.pow (which turns this into a double
+            // little double can not turn into int but big Double can
+            Double exponential = Math.pow(x, exponent);
+
+            //Have to turn back into an integer
+            Integer exponentialInt = exponential.intValue();
+
+            //result = result + exponentialInt
+            result = result + exponentialInt;
+        }
+        return result;
+
 }
+    }
