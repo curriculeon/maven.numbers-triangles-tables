f66f92f5dcd8c1cdfcd997bd911292ac099df8a0
Daniel Boyce <diboyce21students.desu.edu@Daniels-MacBook-Pro-2.local>
2023-01-24 17:13:32 -0500

    finished

--- src/main/java/com/github/curriculeon/TriangleUtilities.java
+++ src/main/java/com/github/curriculeon/TriangleUtilities.java
@@ -3,18 +3,29 @@ package com.github.curriculeon;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String result = "";
+        for (int i = 0; i <numberOfRows; i++) {
+            result+= getRow(i);
+            result+="\n";
+        }
+        return result.stripLeading();
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String result = "";
+        for (int i = 0; i <numberOfStars; i++) {
+            result+="*";
+        }
+        return result;
     }
 
     public static String getSmallTriangle() {
-        return null;
+
+        return getTriangle(4);
     }
 
     public static String getLargeTriangle() {
-        return null;
+
+        return getTriangle(10);
     }
 }
