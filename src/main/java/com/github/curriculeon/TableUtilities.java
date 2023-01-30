1133193b3c228a1bd4b28d8dbf4a45285ff7c807
21mgarbutt <malcolmgarbutt22@gmail.com>
2023-01-30 15:15:33 -0500

    Completed Table Utilities

--- src/main/java/com/github/curriculeon/TableUtilities.java
+++ src/main/java/com/github/curriculeon/TableUtilities.java
@@ -2,14 +2,36 @@ package com.github.curriculeon;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return(getMultiplicationTable(5));
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return(getMultiplicationTable(10));
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String table ="";
+        int size=tableSize;
+        int row=1; //each row of table
+        while(row<=size){   //iterate adding single row at a time to table
+            int count =1; // incrementer for each value in row
+            int num; // var for each value
+            while (count<=size){   // iterate each value in row
+                num = count*row;   //calculating table value for each num
+                if (num<10){    //for correct formatting of spaces
+                    table += ("  "+num+" |"); //single digit num require two leading spaces
+                }
+                else if (num<100){
+                    table += (" "+num+" |"); //double digit num require one leading space
+                }
+                else{
+                    table += (num+" |"); //triple digit num require no leading space
+                }
+                count++; //increment counter for next number
+            }
+            table+="\n"; //format for next row
+            row+=1; //increment row counter
+        }
+        return table;
     }
 }
