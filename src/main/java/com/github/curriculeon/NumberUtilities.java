fa77e1848b34c40d9ca96475cf0e28b02fbcbd42
mhayesCS <97618624+mhayesCS@users.noreply.github.com>
2023-01-30 11:57:40 -0500

    I'm sooo close

--- src/main/java/com/github/curriculeon/NumberUtilities.java
+++ src/main/java/com/github/curriculeon/NumberUtilities.java
@@ -3,34 +3,55 @@ package com.github.curriculeon;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for(int i = start; i < stop; i++)
+            if(i % 2 == 0) result += i;
+        return result;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for(int i = start; i < stop; i++)
+            if(i % 2 != 0) result += i;
+        return result;
     }
 
-
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String result = "";
+        for(int i = start; i < stop; i+= step)
+            result += i * i;
+        return result;
     }
 
     public static String getRange(int start) {
-        return null;
+        String result = "";
+        int counter = 0;
+        while (counter < start ) {
+            result += counter;
+            counter += 1;
+        }
+        return result;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String result = "";
+        for(int i = start; i < stop; i++) result += i;
+        return result;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String result = "";
+        for(int i = start; i < stop; i+= step) result += i;
+        return result;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String result = "";
+        for(int i = start; i < stop; i+= step)
+            result += (int)Math.pow(i, exponent);
+        return result;
     }
 }
