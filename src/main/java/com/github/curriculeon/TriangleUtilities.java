ff4ff92b1a9106398d9f17f6afe1066661748ae5
21mgarbutt <malcolmgarbutt22@gmail.com>
2023-01-30 15:28:17 -0500

    completed

--- src/main/java/com/github/curriculeon/TriangleUtilities.java
+++ src/main/java/com/github/curriculeon/TriangleUtilities.java
@@ -3,18 +3,30 @@ package com.github.curriculeon;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String tri ="";
+        int row_count=1;
+        while (row_count<numberOfRows){
+            tri+=getRow(row_count)+"\n";
+            row_count++;
+        }
+        return tri;
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String row="";
+        int count=0;
+        while(count<numberOfStars){
+            row+="*";
+            count++;
+        }
+        return (row);
     }
 
     public static String getSmallTriangle() {
-        return null;
+        return getTriangle(5);
     }
 
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(10);
     }
 }
