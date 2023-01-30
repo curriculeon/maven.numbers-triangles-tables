ea306767e5369093dd7c68ee6f97f57c194b9f91
wrvargas20students.desu.edu <wrvargas20students.desu.edu>
2023-01-30 10:18:52 -0500

    I have added changes

--- src/main/java/com/github/curriculeon/NumberUtilities.java
+++ src/main/java/com/github/curriculeon/NumberUtilities.java
@@ -3,34 +3,51 @@ package com.github.curriculeon;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String bob = "";
+        for(int i = start; i < stop; i++)
+            if (i % 2 == 0) bob += i;
+        return bob;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String bob2 = "";
+        for(int i = start; i < stop; i++)
+            if (i % 2 == 1) bob2 += i;
+        return bob2;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String bob = "";
+        for(int i = start; i < stop; i++)
+            bob += i*i;
+        return bob;
+
     }
 
     public static String getRange(int start) {
-        return null;
+        String billy = "";
+        for(int i = 0; i < start; i++) billy += i;
+        return billy;
+
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String Collector = "";
+        for (int i = start; i < stop; i++) Collector +=1;
+        return Collector;
     }
 
 
     public static String getRange(int start, int stop, int step) {
+
         return null;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
+
         return null;
     }
 }
