36597772d55097b14b8801db4c36f65210b5a310
AndrewAscone <andrew.ascone@gmail.com>
2023-08-20 16:52:21 -0400

    Number utils complete, tests pass

--- src/main/java/com/github/curriculeon/NumberUtilities.java
+++ src/main/java/com/github/curriculeon/NumberUtilities.java
@@ -3,17 +3,34 @@ package com.github.curriculeon;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        for (int i = start; i < stop; i++) {
+            if(i % 2 == 0){
+                result.append(i);
+            }
+        }
+        return result.toString();
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        for (int i = start; i < stop; i++) {
+            if(i % 2 == 1){
+                result.append(i);
+            }
+        }
+        return result.toString();
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        for (int i = start; i < stop; i+=step) {
+            int square = i * i;
+            result.append(square);
+        }
+        return result.toString();
     }
 
     public static String getRange(int start) {
@@ -34,11 +51,20 @@ public class NumberUtilities {
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        for (int i = start; i < stop; i+=step) {
+            result.append(i);
+        }
+        return result.toString();
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        for (int i = start; i < stop; i+=step) {
+            int x = (int) Math.pow(i, exponent);
+            result.append(x);
+        }
+        return result.toString();
     }
 }
