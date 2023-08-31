bd3728f5197661afb8e0653b6cc7e98c213b2e13
AndrewAscone <andrew.ascone@gmail.com>
2023-08-31 14:15:42 -0400

    Completed Triangle and Table Utils, all tests pass

--- src/main/java/com/github/curriculeon/TableUtilities.java
+++ src/main/java/com/github/curriculeon/TableUtilities.java
@@ -2,14 +2,23 @@ package com.github.curriculeon;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder table = new StringBuilder();
+        for (int i = 1; i < tableSize + 1; i++) {
+            StringBuilder row = new StringBuilder();
+            for (int j = 1; j < tableSize + 1; j++) {
+                row.append(String.format("%3d ", i * j)).append("|");
+            }
+            row.append("\n");
+            table.append(row);
+        }
+        return table.toString();
     }
 }
--- src/main/java/com/github/curriculeon/TriangleUtilities.java
+++ src/main/java/com/github/curriculeon/TriangleUtilities.java
@@ -3,18 +3,34 @@ package com.github.curriculeon;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        //Create a variable to store result (to be returned later)
+        StringBuilder result = new StringBuilder();
+        //Start looping numberOfRow times
+        for(int i = 1; i < numberOfRows; i ++){//Within each iteration:
+            //Create a row with 'iteration_number' of stars
+            String row = getRow(i);
+            //Add newly created row to result
+            result.append(row);
+            //Add new line break to end of row
+            result.append("\n");
+        }//Finish loop
+        //Return result
+        return result.toString();
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < numberOfStars; i++) {
+            result.append("*");
+        }
+        return result.toString();
     }
 
     public static String getSmallTriangle() {
-        return null;
+        return getTriangle(5);
     }
 
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(10);
     }
 }
